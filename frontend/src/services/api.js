03b06fdf587ad9735072454b9734c651dc4ff3a8
Savio-Jos3 <saviojosephsonny3@gmail.com>
2025-10-22 03:15:22 +0530

    Cors corrected

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const API_BASE_URL = 'https://s-mart-o9lm.onrender.com/api';
+const API_BASE_URL = 'http://localhost:5001/api';
 
 const api = axios.create({
   baseURL: API_BASE_URL,
